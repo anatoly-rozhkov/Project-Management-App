ad1e702cdc687bb5fcb1c887618ae49dec0a26e6
anatoly <arozhkov@za-prosto.ru>
2024-11-21 10:26:59 +0300

    Added task submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import logo from '../public/logo.png';
-import React from 'react';
-import AddProject from './components/AddProject';
-import AddTask from './components/AddTask';
-import PaginatedList from './components/PaginatedList';
-
+import logo from "../public/logo.png";
+import React from "react";
+import AddProject from "./components/AddProject";
+import AddTask from "./components/AddTask";
+import PaginatedList from "./components/PaginatedList";
 
 function App() {
   const [editorState, setEditorState] = React.useState("startingState");
@@ -11,42 +10,69 @@ function App() {
   const createTaskRef = React.useRef();
   const [projects, setProjects] = React.useState([]);
 
- function handleEditorState() {
-  if (editorState === "startingState") {
-    setEditorState("projectCreationState");
+  function handleEditorState() {
+    if (editorState === "startingState") {
+      setEditorState("projectCreationState");
+    }
   }
- }
 
- const handleProjectSubmit = (project_data) => {
-  setProjects((projects) => [...projects, project_data]);
-  // createProejectRef.current.querySelector("#discard-button").click();
-  setEditorState("taskCreationState");
-  console.log("Project Data:", projects);
-};
+  const handleProjectSubmit = (project_data) => {
+    project_data.tasks = []
+    setProjects((projects) => [...projects, project_data]);
+    setEditorState("taskCreationState");
+  };
 
-return (
+  const handleTaskSubmit = (task_data) => {
+    projects[projects.length - 1].tasks.push(task_data)
+    createTaskRef.current.querySelector("#task-name").value = "";
+  };
+
+  React.useEffect(() => {
+    console.log("Updated Projects:", projects);
+  }, [projects]);
+
+  return (
     <>
       <div className="flex h-screen">
         <aside className="w-1/4 h-[calc(100vh-2.5rem)] bg-black text-white flex flex-col items-center justify-start gap-4 pt-20 p-4 fixed top-20 rounded-tr-xl">
           <h2>YOUR PROJECTS</h2>
-          <button onClick={handleEditorState} className="px-4 py-2 sm:px-6 sm:py-3 lg:px-8 lg:py-4 bg-stone-700 text-white hover:bg-stone-800 rounded-md transition-all duration-300">+ ADD PROJECT</button>
-          <PaginatedList projects={projects}/>
+          <button
+            onClick={handleEditorState}
+            className="px-4 py-2 sm:px-6 sm:py-3 lg:px-8 lg:py-4 bg-stone-700 text-white hover:bg-stone-800 rounded-md transition-all duration-300"
+          >
+            + ADD PROJECT
+          </button>
+          <PaginatedList projects={projects} />
         </aside>
         <div className="flex-1 flex flex-col items-center justify-center bg-gray-100 gap-4 p-4">
-        {
-          editorState === "startingState" ? (
+          {editorState === "startingState" ? (
             <>
-              <img src={logo} alt="Notepad with a pen" className="w-32 h-auto"/>
+              <img
+                src={logo}
+                alt="Notepad with a pen"
+                className="w-32 h-auto"
+              />
               <h2>No Project Selected</h2>
               <p>Select a project or get started with a new one</p>
-              <button onClick={handleEditorState} className="px-4 py-2 sm:px-6 sm:py-3 lg:px-8 lg:py-4 bg-stone-500 text-white hover:bg-stone-700 rounded-md transition-all duration-300">Create new project</button>
+              <button
+                onClick={handleEditorState}
+                className="px-4 py-2 sm:px-6 sm:py-3 lg:px-8 lg:py-4 bg-stone-500 text-white hover:bg-stone-700 rounded-md transition-all duration-300"
+              >
+                Create new project
+              </button>
             </>
           ) : editorState === "projectCreationState" ? (
-            <AddProject ref={createProejectRef} onSubmit={handleProjectSubmit}/>
+            <AddProject
+              ref={createProejectRef}
+              onSubmit={handleProjectSubmit}
+            />
           ) : (
-            <AddTask ref={createTaskRef} project={projects[0]} onSubmit={handleProjectSubmit}/>
-          )
-        }
+            <AddTask
+              ref={createTaskRef}
+              project={projects[projects.length - 1]}
+              onSubmit={handleTaskSubmit}
+            />
+          )}
         </div>
       </div>
     </>
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -21,41 +21,18 @@ const AddTask = forwardRef(function AddTask({  project, ...props }, ref) {
             <p>{project.date}</p>
             <p>{project.description}</p>
          </div>
-         <form>
+         <form ref={ref} onSubmit={handleSubmit}>
             <div>
                 <button type="submit" className="w-full bg-stone-500 text-white px-4 py-2 rounded-md hover:bg-stone-700 transition duration-300">
                      Add Task
                 </button>
             </div>
-            <div className="mb-4">
+            <div className="mb-4" id={"task-name"}>
                  <label htmlFor="name" className="block text-gray-700 font-medium mb-2">Tasks</label>
                  <input id="name" type="text" name="name" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-stone-500"></input>
             </div>
          </form>
         </>
-
-        // <form ref={ref} onSubmit={handleSubmit} className="w-full max-w-sm mx-auto p-6 bg-white rounded-lg shadow-lg">
-        //     <div className="mb-4">
-        //         <button type="submit" className="w-full bg-stone-500 text-white px-4 py-2 rounded-md hover:bg-stone-700 transition duration-300">
-        //             Add Task
-        //         </button>
-        //         <button id="discard-button" type="reset" className="w-full bg-stone-500 text-white px-4 py-2 rounded-md hover:bg-stone-700 transition duration-300">
-        //             Cancel
-        //         </button>
-        //     </div>
-        //     <div className="mb-4">
-        //         <label htmlFor="title" className="block text-gray-700 font-medium mb-2">TITLE</label>
-        //         <input id="title" type="text" name="title" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-stone-500"></input>
-        //     </div>
-        //     <div className="mb-4">
-        //         <label htmlFor="description" className="block text-gray-700 font-medium mb-2">DESCRIPTION</label>
-        //         <input id="description" type="text" name="description" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-stone-500"></input>
-        //     </div>
-        //     <div className="mb-4">
-        //         <label htmlFor="date" className="block text-gray-700 font-medium mb-2">DUE DATE</label>
-        //         <input id="date" type="date" name="date" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-stone-500"></input>
-        //     </div>
-        // </form>
     );
 })
 
